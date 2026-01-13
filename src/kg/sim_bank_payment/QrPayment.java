ed090380cc8d5c53492be88125bed6b7ab16ec8b
sanzhar807 <amanturov2471@gmail.com>
2026-01-13 10:48:00 +0700

    abc

---
+++ src/kg/sim_bank_payment/QrPayment.java
@@ -0,0 +1,8 @@
+package kg.sim_bank_payment;
+
+public class QrPayment {
+    public static void main(String[] args) {
+        System.out.println("QR Payment");
+    }
+}
+//   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
