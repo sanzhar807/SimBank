7e268a9b30d14f356ea0b5c300ac8e60be2c4b24
sanzhar807 <amanturov2471@gmail.com>
2026-01-13 11:29:40 +0700

    Master Visa class

--- src/kg/sim_bank_payment/MasterCardPayment.java
+++ src/kg/sim_bank_payment/MasterCardPayment.java
@@ -3,5 +3,6 @@ package kg.sim_bank_payment;
 public class MasterCardPayment {
     public static void main(String[] args) {
         System.out.println("Master");
+        System.out.println("sss");
     }
 }
--- src/kg/sim_bank_payment/VisaPayment.java
+++ src/kg/sim_bank_payment/VisaPayment.java
@@ -1,4 +1,7 @@
 package kg.sim_bank_payment;
 
 public class VisaPayment {
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
 }
