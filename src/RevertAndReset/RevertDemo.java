70b56cdf22b8a6c1f9c6a833530e8212f6e60915
sanzhar807 <amanturov2471@gmail.com>
2026-01-19 19:25:05 +0700

    Revert "my name"
    This reverts commit d7827449feee0391c041a3a5013f6a64d0409f37.

--- src/RevertAndReset/RevertDemo.java
+++ src/RevertAndReset/RevertDemo.java
@@ -3,6 +3,5 @@ package RevertAndReset;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println(My name is not Ali);
     }
 }
