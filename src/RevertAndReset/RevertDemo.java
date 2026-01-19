d7827449feee0391c041a3a5013f6a64d0409f37
sanzhar807 <amanturov2471@gmail.com>
2026-01-19 19:17:07 +0700

    my name

--- src/RevertAndReset/RevertDemo.java
+++ src/RevertAndReset/RevertDemo.java
@@ -3,5 +3,6 @@ package RevertAndReset;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println(My name is not Ali);
     }
 }
